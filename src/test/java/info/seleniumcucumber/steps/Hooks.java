b68e5872f13a5239d61e048fdc1d72c917bfd582
keerthi <keerthikolli86@gmail.com>
2024-04-08 16:13:23 +0100

    screenshot added in hooks

--- src/test/java/info/seleniumcucumber/steps/Hooks.java
+++ src/test/java/info/seleniumcucumber/steps/Hooks.java
@@ -1,29 +1,30 @@
 package info.seleniumcucumber.steps;
 
+import info.seleniumcucumber.utils.ScreenShotMethods;
 import io.cucumber.core.api.Scenario;
 import io.cucumber.java.After;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class Hooks {
     private final Logger log = LoggerFactory.getLogger(Hooks.class);
 
     @After
-    public void afterScenario(Scenario scenario) {
+    public void afterScenario(Scenario scenario) throws IOException {
         endOfTest(scenario);
     }
 
-    public void endOfTest(Scenario scenario) {
+    public void endOfTest(Scenario scenario) throws IOException {
         if (scenario.getStatus() != null && scenario.isFailed()) {
-            String filename = scenario.getName().replaceAll("\\s+", "_");
-            final String featureError = scenario.getId().replaceAll("\\s+", "_").replaceAll(":", "_").split("\\.")[1];
-            filename = filename + "_" + featureError;
-            scenario.embed(filename.getBytes(StandardCharsets.UTF_8), "image/png", filename);
+            ScreenShotMethods screenshotObj = new ScreenShotMethods();
+            screenshotObj.takeScreenShot();
+            log.info("" + scenario.isFailed());
         }
 
-        log.info("");
+
         log.info("==========================================================================");
         log.info("================================Test " + scenario.getStatus().toString() +"===============================");
         log.info("==========================================================================");
